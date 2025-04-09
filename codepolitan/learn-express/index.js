671f728a517b141f83fa787edf0484d06f2c22e2
Sandysan1234 <rahmahdiandian@gmail.com>
2025-04-09 21:51:24 +0700

    update routing wpu dan update belajar codepolitan

--- codepolitan/learn-express/index.js
+++ codepolitan/learn-express/index.js
@@ -1,17 +1,35 @@
-const express = require ('express')
-const port = 3000
-const app = express ()
+const express = require("express");
+const app = express();
+const port = 3000;
 
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+app.get("/about/:cat", (req, res) => {
+  const { cat } = req.params;
+  res.send(`halaman about! ${cat}`);
+});
+// route params start
+app.get("/blog/:category/:judul/:author", (req, res) => {
+  const { category, judul, author } = req.params;
 
+  res.send(
+    `blog ini memiliki category : ${category}, author ${author} adalah ${judul}`
+  );
+});
+// route params end
+//route query start
+app.get("/about/:cat", (req, res) => {
+  const { cat } = req.params;
+  res.send(`halaman about! ${cat}`);
+});
+//route query end
+app.get("/user", (req, res) => {
+  const { q } = req.query;
+  res.send(`halo ${q}`);
+  console.log({ q });
+});
 
-app.get('/', (req,res) =>{
-    res.send('makan bang')
-})
-app.get('/cats', (req,res) =>{    
-    res.send('makan kucing')
-})
-
-app.listen(port, () =>{
-    console.log(`app listen http://localhost:${port}`);
-    
-})
+app.listen(port, () => {
+  console.log(`app listen http://localhost:${port}`);
+});
---
+++ wpu/index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.sendFile("./index.html", { root: __dirname });
+});
+
+app.get("/about", (req, res) => {
+  res.sendFile("./about.html", { root: __dirname });
+});
+
+app.get("/contact", (req, res) => {
+  res.sendFile("./contact.html", { root: __dirname });
+});
+
+app.get("/produk", (req, res) => {
+  res.send(`product id`, req.params);
+});
+
+app.use("/", (req, res) => {
+  res.status(404);
+  res.send("<h1>halaman tidak ditemukan</h1>");
+});
+
+app.listen(port, () => {
+  console.log(`app listen http://localhost:${port}`);
+});
