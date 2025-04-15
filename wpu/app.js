2290e92b5b9ee5c76a9724f57a6fd46cd1c656de
sandysan1234 <rahmahdiandian@gmail.com>
2025-04-15 14:30:12 +0700

    layouting

--- wpu/app.js
+++ wpu/app.js
@@ -1,24 +1,45 @@
 const express = require("express");
+const expressLayouts = require('express-ejs-layouts');
 const app = express();
 const port = 3000;
 
 app.set("view engine", "ejs");
+app.use(expressLayouts)
 
 app.get("/", (req, res) => {
+  const barang = [
+    {
+      nama: "minyak goreng",
+      qty: "25",
+      harga: 25000
+    },
+    {
+      nama: "sabun cuci piring",
+      qty: 20,
+      harga: 250000
+    },
+    {
+      nama: "mie goreng",
+      qty: "1 dus",
+      harga: 45000
+    },
+  ];
 
-  
-  // res.render("./index.html", { root: __dirname });
-  res.render("index", { nama: "Rizky Sandy", title :'Halaman Home' });
+  res.render("index", { 
+    nama: "Rizky Sandy", 
+    title: "Halaman Home",
+    barang,
+   });
 });
 
 app.get("/about", (req, res) => {
   // res.sendFile("./about.html", { root: __dirname });
-  res.render("about");
+  res.render("about", {title:"Halaman About"});
 });
 
 app.get("/contact", (req, res) => {
   // res.sendFile("./contact.html", { root: __dirname });
-  res.render("contact");
+  res.render("contact", {title:"Halaman Contact"});
 });
 
 app.get("/produk/:id", (req, res) => {
